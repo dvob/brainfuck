656e1c37e018ba5075a87349db5b36b5d48a0736
David Schneider <dsbrng25b@gmail.com>
2021-09-20 15:23:11 +0200

    Add more tests and add over/under-flow

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, u128::MAX};
 
 #[derive(Debug,PartialEq)]
 enum Command {
@@ -51,10 +51,22 @@ impl Engine {
         let mut output = String::new();
         for command in commands {
             match command {
-                Command::Increment => self.data[self.pos] += 1,
-                Command::Decrement => self.data[self.pos] -= 1,
+                Command::Increment => {
+                    if self.data[self.pos] == u8::MAX {
+                        self.data[self.pos] = 0
+                    } else {
+                        self.data[self.pos] += 1
+                    }
+                },
+                Command::Decrement => {
+                    if self.data[self.pos] == 0 {
+                        self.data[self.pos] = u8::MAX;
+                    } else {
+                        self.data[self.pos] -= 1
+                    }
+                }
                 Command::MoveRight => {
-                    if self.pos == MAX_POSITIONS {
+                    if self.pos == MAX_POSITIONS-1 {
                         self.pos = 0 
                     } else {
                         self.pos += 1
@@ -62,12 +74,14 @@ impl Engine {
                 },
                 Command::MoveLeft => {
                     if self.pos == 0 {
-                        self.pos = MAX_POSITIONS
+                        self.pos = MAX_POSITIONS-1
                     } else {
                         self.pos -= 1
                     }
                 },
-                Command::Print => output.push(self.data[self.pos].into()),
+                Command::Print => {
+                    output.push(self.data[self.pos].into());
+                },
                 Command::Read => todo!(),
                 Command::Loop(loop_commands) => {
                     while self.data[self.pos] != 0 {
@@ -75,9 +89,57 @@ impl Engine {
                     }
                 }
             }
+            //println!("pos={}, cmd={:?}", self.pos, command);
         }
         Ok(output)
     }
+}
+
+#[test]
+fn test_pos_overflow() -> Result<(),Box<dyn Error>> {
+    let input = ">+";
+    let commands = parse_commands(&mut input.chars())?;
+    let mut engine = Engine::new();
+    engine.pos = MAX_POSITIONS-1;
+    engine.execute(&commands)?;
+    assert_eq!(engine.pos, 0);
+    assert_eq!(engine.data[0], 1);
+    Ok(())
+}
+
+#[test]
+fn test_pos_underflow() -> Result<(),Box<dyn Error>> {
+    let input = "<+";
+    let commands = parse_commands(&mut input.chars())?;
+    let mut engine = Engine::new();
+    engine.execute(&commands)?;
+    assert_eq!(engine.pos, MAX_POSITIONS-1);
+    assert_eq!(engine.data[MAX_POSITIONS-1], 1);
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn test_loop() -> Result<(),Box<dyn Error>> {
+    let input = ">+[>+]+";
+    let commands = parse_commands(&mut input.chars())?;
+    assert_eq!(commands, vec![
+        Command::MoveRight,
+        Command::Increment,
+        Command::Loop(vec![
+            Command::MoveRight,
+            Command::Increment,
+        ]),
+        Command::Increment,
+    ]);
+    let mut engine = Engine::new();
+    let _ = engine.execute(&commands)?;
+    for i in 0..(MAX_POSITIONS-1) {
+        if engine.data[i] != 1 {
+            panic!("index={}, value != 1", i);
+        }
+    }
+    Ok(())
 }
 
 #[test]
