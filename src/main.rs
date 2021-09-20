d703b9b8083e6b7e0386b677a15abb93275578cf
David Schneider <dsbrng25b@gmail.com>
2021-09-20 16:41:47 +0200

    Fix output and extend tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::{error::Error, u128::MAX};
+use std::error::Error;
+use std::io::BufWriter;
+use std::io::Write;
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 enum Command {
     MoveRight,
     MoveLeft,
@@ -11,8 +13,9 @@ enum Command {
     Loop(Vec<Command>),
 }
 
-fn parse_commands<I>(input: &mut I) -> Result<Vec<Command>, Box<dyn Error>> 
-where I: Iterator<Item=char>,
+fn parse_commands<I>(input: &mut I) -> Result<Vec<Command>, Box<dyn Error>>
+where
+    I: Iterator<Item = char>,
 {
     let mut commands = Vec::new();
     while let Some(c) = input.next() {
@@ -47,8 +50,11 @@ impl Engine {
         }
     }
 
-    fn execute(&mut self, commands: &Vec<Command>) -> Result<String, Box<dyn Error>> {
-        let mut output = String::new();
+    fn execute<W: Write>(
+        &mut self,
+        commands: &[Command],
+        out: &mut W,
+    ) -> Result<(), Box<dyn Error>> {
         for command in commands {
             match command {
                 Command::Increment => {
@@ -57,7 +63,7 @@ impl Engine {
                     } else {
                         self.data[self.pos] += 1
                     }
-                },
+                }
                 Command::Decrement => {
                     if self.data[self.pos] == 0 {
                         self.data[self.pos] = u8::MAX;
@@ -66,107 +72,101 @@ impl Engine {
                     }
                 }
                 Command::MoveRight => {
-                    if self.pos == MAX_POSITIONS-1 {
-                        self.pos = 0 
+                    if self.pos == MAX_POSITIONS - 1 {
+                        self.pos = 0
                     } else {
                         self.pos += 1
                     }
-                },
+                }
                 Command::MoveLeft => {
                     if self.pos == 0 {
-                        self.pos = MAX_POSITIONS-1
+                        self.pos = MAX_POSITIONS - 1
                     } else {
                         self.pos -= 1
                     }
-                },
+                }
                 Command::Print => {
-                    output.push(self.data[self.pos].into());
-                },
+                    out.write_all(&[self.data[self.pos]])?;
+                }
                 Command::Read => todo!(),
                 Command::Loop(loop_commands) => {
                     while self.data[self.pos] != 0 {
-                        self.execute(&loop_commands)?;
+                        self.execute(loop_commands, out)?;
                     }
                 }
             }
-            //println!("pos={}, cmd={:?}", self.pos, command);
         }
-        Ok(output)
+        Ok(())
     }
 }
 
 #[test]
-fn test_pos_overflow() -> Result<(),Box<dyn Error>> {
+fn test_pos_overflow() -> Result<(), Box<dyn Error>> {
+    let mut buf = BufWriter::new(Vec::new());
     let input = ">+";
     let commands = parse_commands(&mut input.chars())?;
     let mut engine = Engine::new();
-    engine.pos = MAX_POSITIONS-1;
-    engine.execute(&commands)?;
+    engine.pos = MAX_POSITIONS - 1;
+    engine.execute(&commands, &mut buf)?;
     assert_eq!(engine.pos, 0);
     assert_eq!(engine.data[0], 1);
     Ok(())
 }
 
 #[test]
-fn test_pos_underflow() -> Result<(),Box<dyn Error>> {
+fn test_pos_underflow() -> Result<(), Box<dyn Error>> {
+    let mut buf = BufWriter::new(Vec::new());
     let input = "<+";
     let commands = parse_commands(&mut input.chars())?;
     let mut engine = Engine::new();
-    engine.execute(&commands)?;
-    assert_eq!(engine.pos, MAX_POSITIONS-1);
-    assert_eq!(engine.data[MAX_POSITIONS-1], 1);
+    engine.execute(&commands, &mut buf)?;
+    assert_eq!(engine.pos, MAX_POSITIONS - 1);
+    assert_eq!(engine.data[MAX_POSITIONS - 1], 1);
     Ok(())
 }
 
 #[test]
-#[ignore]
-fn test_loop() -> Result<(),Box<dyn Error>> {
-    let input = ">+[>+]+";
+fn test_hello_world() -> Result<(), Box<dyn Error>> {
+    let mut buf = BufWriter::new(Vec::new());
+    let input = "+[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-.";
     let commands = parse_commands(&mut input.chars())?;
-    assert_eq!(commands, vec![
-        Command::MoveRight,
-        Command::Increment,
-        Command::Loop(vec![
-            Command::MoveRight,
-            Command::Increment,
-        ]),
-        Command::Increment,
-    ]);
     let mut engine = Engine::new();
-    let _ = engine.execute(&commands)?;
-    for i in 0..(MAX_POSITIONS-1) {
-        if engine.data[i] != 1 {
-            panic!("index={}, value != 1", i);
-        }
-    }
+    engine.execute(&commands, &mut buf)?;
+    let output = String::from_utf8(buf.into_inner()?)?;
+    assert_eq!(output, "hello world");
     Ok(())
 }
 
 #[test]
-fn test() -> Result<(),Box<dyn Error>> {
+fn test() -> Result<(), Box<dyn Error>> {
+    let mut buf = BufWriter::new(Vec::new());
     let input = "+>++<-";
     let commands = parse_commands(&mut input.chars())?;
-    assert_eq!(commands, vec![
-        Command::Increment,
-        Command::MoveRight,
-        Command::Increment,
-        Command::Increment,
-        Command::MoveLeft,
-        Command::Decrement,
-    ]);
+    assert_eq!(
+        commands,
+        vec![
+            Command::Increment,
+            Command::MoveRight,
+            Command::Increment,
+            Command::Increment,
+            Command::MoveLeft,
+            Command::Decrement,
+        ]
+    );
     let mut engine = Engine::new();
-    let _ = engine.execute(&commands)?;
+    engine.execute(&commands, &mut buf)?;
+    let output = String::from_utf8(buf.into_inner()?)?;
     assert_eq!(engine.data[0], 0);
     assert_eq!(engine.data[1], 2);
+    assert_eq!(output, "");
     Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let file_name: String = std::env::args().skip(1).next().unwrap();
+    let file_name: String = std::env::args().nth(1).unwrap();
     let input = std::fs::read_to_string(file_name)?;
     let commands = parse_commands(&mut input.chars())?;
     let mut engine = Engine::new();
-    let output = engine.execute(&commands)?;
-    println!("{}", output);
+    engine.execute(&commands, &mut std::io::stdout())?;
     Ok(())
 }
